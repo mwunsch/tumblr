5fe607f812784dc82d769afdcc42a7da51c2ea5a
Mark Wunsch <mark@markwunsch.com>
2010-03-26 13:45:21 -0400

    Version bump to v1.2.0

--- lib/tumblr.rb
+++ lib/tumblr.rb
@@ -1,7 +1,7 @@
 require 'weary'
 
 class Tumblr
-  VERSION = "1.1.1"
+  VERSION = "1.2.0"
   GENERATOR = "The Tumblr Gem v#{VERSION}"
   USER_AGENT = "TumblrGem/#{VERSION} (+http://github.com/mwunsch/tumblr)"
   
