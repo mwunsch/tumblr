afcc89cfeedd3c0c7ff01935f86fb1d85ffc398a
Mark Wunsch <mark@markwunsch.com>
2012-09-26 12:38:45 -0400

    Messing w/ Thor options

--- lib/tumblr/command_line_interface.rb
+++ lib/tumblr/command_line_interface.rb
@@ -3,15 +3,16 @@ require 'thor'
 module Tumblr
   class CommandLineInterface < Thor
 
+    class_option :credentials, :type => :string,
+                               :desc => "The file path where your Tumblr OAuth keys are stored. Defaults to ~/.tumblr."
+
     desc "post", "Posts a photo from a url to tumblr"
-    def post(host, url)
-      require "tumblr/credentials"
-      cred = Tumblr::Credentials.new.read
-      invoke :authorize if cred.empty?
-      client = Tumblr::Client.load(host)
-      photo = Tumblr::Post::Photo.new(:source => url, :caption => "I posted this using the bleeding edge of the new Tumblr Command Line utility for #fashionhack day.")
-      response = photo.post(client).perform
-      puts response.body
+    def post
+      abort "No, dude. No." unless has_credentials?
+      # client = Tumblr::Client.load(host)
+      # photo = Tumblr::Post::Photo.new(:source => url, :caption => "I posted this using the bleeding edge of the new Tumblr Command Line utility for #fashionhack day.")
+      # response = photo.post(client).perform
+      # puts response.body
     end
 
     desc "authorize", "Authenticate and authorize the cli"
@@ -23,12 +24,29 @@ module Tumblr
       end
     end
 
+    desc "version", "Print Tumblr version information"
+    def version
+      puts Tumblr::VERSION
+    end
+
     def help
       if !$stdin.tty?
         puts "you put something in the $stdin"
+        invoke post
       else
         super
       end
+    end
+
+    private
+
+    def credentials
+      require 'tumblr/credentials'
+      Tumblr::Credentials.new(options[:credentials]).read
+    end
+
+    def has_credentials?
+      !credentials.empty?
     end
 
   end
