23e928bad6e274346fa8646bf072da0fce5f31ee
Mark Wunsch <mark@markwunsch.com>
2012-09-27 00:29:09 -0400

    Working on the cli. Auth and piping

--- lib/tumblr/command_line_interface.rb
+++ lib/tumblr/command_line_interface.rb
@@ -1,26 +1,60 @@
 require 'thor'
+require 'tumblr'
 
 module Tumblr
   class CommandLineInterface < Thor
+    include Thor::Actions
+
+    default_task :pipe
 
     class_option :credentials, :type => :string,
                                :desc => "The file path where your Tumblr OAuth keys are stored. Defaults to ~/.tumblr."
 
+    check_unknown_options!(:except => [])
+
+
     desc "post", "Posts a photo from a url to tumblr"
-    def post
+    option :host, :type => :string,
+                  :desc => "The hostname of the blog you want to post to"
+    def post(arg)
+      if options[:host].nil?
+        host = ask("Hostname plz?") if $stdin.tty?
+      end
+      host ||= options[:host]
+      abort "You need a hostname." if host.nil? or host.empty?
       abort "No, dude. No." unless has_credentials?
-      # client = Tumblr::Client.load(host)
-      # photo = Tumblr::Post::Photo.new(:source => url, :caption => "I posted this using the bleeding edge of the new Tumblr Command Line utility for #fashionhack day.")
-      # response = photo.post(client).perform
+      client = Tumblr::Client.load hostname, options[:credentials]
+      # TODO: Infer post type from arg and convert to post
+      #   or, if arg is a path, read it from a file
+      # file = File.read(File.expand_path(path))
+      # post = Tumblr::Post.load(file)
+      # puts post.serialize
+      # response = post.post(client).perform
       # puts response.body
     end
 
     desc "authorize", "Authenticate and authorize the cli"
+    long_desc <<-LONGDESC
+      `tumblr authorize` will start up a server to run an app to do the OAuth handshake with tumblr.
+    LONGDESC
+    option :port, :type => :string,
+                  :default => "4567"
+    option :host, :type => :string,
+                  :default => "0.0.0.0"
     def authorize(*soak)
       require 'tumblr/authentication'
-      # {:port => 4567, :bind => "0.0.0.0"}
-      Tumblr::Authentication.run!() do |server|
-        `open http://0.0.0.0:4567/`
+      sinatra_options = {
+        :port => options[:port],
+        :bind => options[:host],
+        :credential_path => options[:credentials]
+      }
+      Tumblr::Authentication.run!(sinatra_options) do |server|
+        `open http://#{options[:host]}:#{options[:port]}/`
+      end
+      if has_credentials?
+        puts "Great success! Your Tumblr OAuth credentials were written to #{credentials.path}"
+      else
+        abort "Something went wrong in authorization, and credentials were not correctly written to #{credentials.path}"
       end
     end
 
@@ -29,12 +63,14 @@ module Tumblr
       puts Tumblr::VERSION
     end
 
-    def help
+    desc "pipe", "Pipe post content in from STDIN"
+    option :host, :type => :string,
+                  :desc => "The hostname of the blog you want to post to"
+    def pipe
       if !$stdin.tty?
-        puts "you put something in the $stdin"
-        invoke post
+        post($stdin.read)
       else
-        super
+        invoke :help
       end
     end
 
@@ -42,11 +78,11 @@ module Tumblr
 
     def credentials
       require 'tumblr/credentials'
-      Tumblr::Credentials.new(options[:credentials]).read
+      Tumblr::Credentials.new(options[:credentials])
     end
 
     def has_credentials?
-      !credentials.empty?
+      !credentials.read.empty?
     end
 
   end
