d9ceeb55fefaf036a9073cd050f1287f77b5fdb0
Mark Wunsch <mark@markwunsch.com>
2012-09-30 20:59:54 -0400

    Edit posts action from the cli.
    This code is hideous.

--- lib/tumblr/command_line_interface.rb
+++ lib/tumblr/command_line_interface.rb
@@ -25,9 +25,7 @@ module Tumblr
                           :aliases => "-d"
     def post(arg)
       check_credentials
-      host = ask("What is your Tumblr hostname?") if options[:host].nil? and $stdin.tty?
-      host ||= options[:host]
-      abort "You need to provide a hostname i.e. --host=YOUR-NAME.tumblr.com" if host.nil? or host.empty?
+      host = get_host
       client = Tumblr::Client.load host, options[:credentials]
       post =  if arg.respond_to? :read
                 Tumblr::Post.load arg.read
@@ -44,6 +42,45 @@ module Tumblr
         puts %Q(Post was successfully created! Post ID: #{response.parse["response"]["id"]})
       else
         abort %Q(Tumblr returned an Error #{response.status}: #{response.parse["response"]["errors"].join})
+      end
+    end
+
+    desc "edit", "Edit a post"
+    long_desc "Open up your $EDITOR to edit a published post."
+    def edit(id)
+      check_credentials
+      host = get_host
+      client = Tumblr::Client.load host, options[:credentials]
+      get_post_response = client.posts(:id => id, :filter => :raw).perform
+      post =  if get_post_response.success?
+                begin
+                  Tumblr::Post.create(get_post_response.parse["response"]["posts"].first)
+                rescue
+                  abort "#{$!}: #{$@}"
+                end
+              else
+                abort "Something went wrong fetching the post."
+              end
+      require 'tempfile'
+      tmp_file = Tempfile.new("post_#{id}")
+      begin
+        tmp_file.write(post.serialize)
+        tmp_file.rewind
+        edited_post = if system "$EDITOR #{tmp_file.path}"
+                        Tumblr::Post.load_from_path tmp_file.path
+                      else
+                        abort "Something went seriously wrong."
+                      end
+        if edited_post.edit(client).perform.success?
+          puts "Post #{id} successfully edited."
+        else
+          abort "Something went wrong when editing the post."
+        end
+      rescue
+        abort "#{$!}: #{$@}"
+      ensure
+        tmp_file.close
+        tmp_file.unlink
       end
     end
 
@@ -105,6 +142,13 @@ module Tumblr
 
     def check_credentials
       abort "Unable to find your OAuth keys. Run `tumblr authorize` to authenticate with Tumblr." unless has_credentials?
+    end
+
+    def get_host
+      host = ask("What is your Tumblr hostname?") if options[:host].nil? and $stdin.tty?
+      host ||= options[:host]
+      abort "You need to provide a hostname i.e. --host=YOUR-NAME.tumblr.com" if host.nil? or host.empty?
+      host
     end
 
   end
